444bfbbac09bbd5643442c933c1f7c5b76c990aa
ChrisAchinga <achinga.chris@gmail.com>
2021-06-15 13:09:31 +0300

    getstatic() prop

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-export default function Home() {
+export default function Home({ posts }) {
+  console.log('POSTS', posts)
   return (
     <div>
       <h1>My Hashnode Articles</h1>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      posts: [],
+    },
+  }
 }
